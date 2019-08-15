de16f53dc22f90cb73efa56afd91e83440d9962a
bobo <haiya201405@gmail.com>
2019-08-15 16:10:17 +0800

    添加cqssc

--- ch02/names/2.py
+++ ch02/names/2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas import pivot_table
 import numpy as np
+import sys
 years = range(1880, 2011)
 pieces = []
 columns = ['name', 'sex', 'births']
@@ -11,11 +12,12 @@ for year in years:
     pieces.append(frame)
 
 names = pd.concat(pieces, ignore_index=True)
-# print names[:2]
+# print names[:200][names[:200].sex == 'F']
+# sys.exit(0)
 total_births = pivot_table(names,
                            values='births',
                            columns='sex',
-                           ndex=['year'],
+                           index=['year'],
                            aggfunc='sum')
 
 
@@ -30,14 +32,18 @@ np.allclose(names.groupby(['year', 'sex']).prop.sum(), 1)
 
 
 def get_top1000(group):
-    return group.sort_index(by="births", ascending=False)[:1000]
+    return group.sort_values(by="births", ascending=False)[:1000]
 
 
 grouped = names.groupby(['year', 'sex'])
 top1000 = grouped.apply(get_top1000)
 pieces = []
 for year, group in names.groupby(['year', 'sex']):
-    pieces.append(group.sort_index(by='births', scending=False)[:1000])
+    pieces.append(group.sort_values(by='births', ascending=False)[:1000])
 top1000 = pd.concat(pieces, ignore_index=True)
 
-print top1000[:2]
+# print top1000[:2]
+boys = top1000[top1000.sex == 'M']
+girls = top1000[top1000.sex == 'F']
+total_births = top1000.groupby(["year", "sex"])
+print total_births
